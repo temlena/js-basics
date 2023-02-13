5af95100769bda598a1f7b3cf526239c61403957
temlena <oridupahelen@gmail.com>
2023-02-13 16:30:53 +0100

    introduction to javascript basics

---
+++ index.js
@@ -0,0 +1,13 @@
+function MULTIPLY(number1, number2) {
+    return (number1 * number2);
+}
+console.log(MULTIPLY(10, 4));
+
+
+const firstName = "Helen";
+const lastName = "Oridupa";
+const stuternTrack = "Software Engineering";
+const skills = "programming,critical thinking";
+const color = "blue"
+const year = 1960
+console.log("My name is "+ firstName + lastName.);
